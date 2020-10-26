3f1f96222a17b2f073685eee2b3d1aa704510740
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:37:39 +0700

    create simpanan bloc

---
+++ lib/bloc/bloc_simpanan.dart
@@ -0,0 +1,14 @@
+part of 'bloc.dart';
+
+class SimpananBloc implements Bloc {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
+
+}
