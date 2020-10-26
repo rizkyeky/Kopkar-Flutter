2299b6e442ac8c355cd523073da71316600d35cc
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:38:30 +0700

    create home page

---
+++ lib/view/page/page_home.dart
@@ -0,0 +1,17 @@
+part of 'page.dart';
+
+class HomePage extends Page<HomeBloc> {
+
+  @override
+  void dispose() {}
+
+  @override
+  void init() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
