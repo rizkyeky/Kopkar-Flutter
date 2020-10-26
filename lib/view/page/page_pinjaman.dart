6b67d7909b94d0b5fc6a0c4935cea56fc8eeee53
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:38:42 +0700

    create pinjaman page

---
+++ lib/view/page/page_pinjaman.dart
@@ -0,0 +1,17 @@
+part of 'page.dart';
+
+class PinjamanPage extends Page<PinjamanBloc> {
+
+  @override
+  void dispose() {}
+
+  @override
+  void init() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
