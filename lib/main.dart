1867ee88e0993f8932b60916121bdbe8dfe15af5
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 20:45:07 +0700

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'router.dart';
+import 'locator.dart';
 import 'theme.dart';
+import 'view/page/page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await setLocator();
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Kopkar',
       theme: appTheme,
-      initialRoute: '/',
-      onGenerateRoute: generatorRouter,
+      home: MainPage(),
     );
   }
 }
