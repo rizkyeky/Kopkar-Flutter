30b6382a67bbecc886a5248fdf37bdc2ebf87348
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:38:36 +0700

    create login page

---
+++ lib/view/page/page_login.dart
@@ -0,0 +1,17 @@
+part of 'page.dart';
+
+class LoginPage extends Page<LoginBloc> {
+
+  @override
+  void dispose() {}
+
+  @override
+  void init() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
