20c7ffd2bfce22ca3d85e11f91bffac363fa879a
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:38:25 +0700

    create history page

---
+++ lib/view/page/page_history.dart
@@ -0,0 +1,17 @@
+part of 'page.dart';
+
+class HistoryPage extends Page<HistoryBloc> {
+
+  @override
+  void dispose() {}
+
+  @override
+  void init() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
