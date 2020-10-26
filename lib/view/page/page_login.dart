403646ca18fdb8bc72c89e06e037a2945367da9c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 22:10:10 +0700

    add widgets

--- lib/view/page/page_login.dart
+++ lib/view/page/page_login.dart
@@ -8,10 +8,43 @@ class LoginPage extends Page<LoginBloc> {
   @override
   void init() {}
 
+  final TextEditingController _emailText = TextEditingController();
+  final TextEditingController _passwordText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        
+      ),
+      body: Builder(
+        builder: (contextScaffold) => SingleChildScrollView(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const SizedBox(height: 36,),
+              XTextField(
+                controller: _emailText,
+                text: 'Email',
+              ),
+              const SizedBox(height: 36,),
+              XTextField(
+                obscureText: true,
+                controller: _passwordText,
+                text: 'Password',
+              ),
+              const SizedBox(height: 36,),
+              
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {}, 
+        label: Text('Login', style: appTheme.textTheme.headline2.copyWith(),)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
