9cc80f1d16f41f885992d5ed697434d056ecea23
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 22:08:28 +0700

    update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 Color primaryColor = Colors.blue;
-Color accentColor = Colors.blue.withOpacity(0.6);
 Color borderColor = Colors.black.withOpacity(0.12);
 Color iconColor = Colors.black.withOpacity(0.54);
 
 ThemeData appTheme = ThemeData(
   brightness: Brightness.light,
   primaryColor: primaryColor,
-  accentColor: accentColor,
+  accentColor: primaryColor,
   visualDensity: VisualDensity.adaptivePlatformDensity,
   iconTheme: IconThemeData(
     color: iconColor,
