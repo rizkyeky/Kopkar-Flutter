4c5f7dfa601a4d2525b65dbc8a7dcc6c4e8de3a4
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:51:55 +0700

    create main page for tab navigator

---
+++ lib/view/page/page_main.dart
@@ -0,0 +1,79 @@
+part of 'page.dart';
+
+class MainPage extends StatelessWidget {
+  
+  final Page homePage = HomePage();
+  final Page pinjamanPage = PinjamanPage();
+  final Page simpananPage = SimpananPage();
+  final Page historyPage = HistoryPage();
+  final Page profilePage = ProfilePage();
+
+  final List<Widget> pages = [];
+
+  final ValueNotifier<int> setTab = ValueNotifier(0);
+
+  MainPage({
+    Key key,
+  }) : super(key: key) {
+    pages.addAll([
+      homePage, 
+      pinjamanPage, 
+      simpananPage, 
+      historyPage, 
+      profilePage
+    ]);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    int prevTab = setTab.value;
+    return Scaffold(
+      body: ValueListenableBuilder<int>(
+        valueListenable: setTab, 
+        builder: (context, value, _) => PageTransitionSwitcher(
+          reverse: value > prevTab,
+          transitionBuilder: (child, primaryAnimation, secondaryAnimation) => 
+            SharedAxisTransition(
+              animation: primaryAnimation,
+              secondaryAnimation: secondaryAnimation,
+              transitionType: SharedAxisTransitionType.horizontal,
+              child: child,
+            ),
+          child: pages[value]
+        )
+      ),
+      bottomNavigationBar: ValueListenableBuilder<int>(
+        valueListenable: setTab,
+        builder: (context, value, child) => BottomNavigationBar(
+          currentIndex: value,
+          selectedIconTheme: IconThemeData(size: 28, color: primaryColor),
+          unselectedIconTheme: IconThemeData(size: 24, color: borderColor),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore_outlined), label: 'Pinjaman',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark_border), label: 'Simpanan',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history_edu_outlined), label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_outlined), label: 'Profile',
+            ),
+          ],
+          onTap: (index) {
+            if (setTab.value != index) {
+              prevTab = setTab.value;
+              setTab.value = index;
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+}
