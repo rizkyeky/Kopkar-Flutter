7d40f721ffa70c6da8adac2eaf13051d7d940f75
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 20:45:16 +0700

    fix and update theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,17 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 Color primaryColor = Colors.blue;
-Color accentColor = Colors.blue.withOpacity(54);
-Color borderColor = Colors.black.withOpacity(12);
+Color accentColor = Colors.blue.withOpacity(0.6);
+Color borderColor = Colors.black.withOpacity(0.12);
+Color iconColor = Colors.black.withOpacity(0.54);
 
 ThemeData appTheme = ThemeData(
   brightness: Brightness.light,
   primaryColor: primaryColor,
   accentColor: accentColor,
   visualDensity: VisualDensity.adaptivePlatformDensity,
+  iconTheme: IconThemeData(
+    color: iconColor,
+  ),
   textTheme: const TextTheme(
     overline: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
-    headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.w700),
     headline6: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
   ),
 );
