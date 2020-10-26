4c28fd4029d2e2f323cd36041f8d9676d7c359b8
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 17:38:47 +0700

    create profile page

---
+++ lib/view/page/page_profile.dart
@@ -0,0 +1,17 @@
+part of 'page.dart';
+
+class ProfilePage extends Page<ProfileBloc> {
+
+  @override
+  void dispose() {}
+
+  @override
+  void init() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
