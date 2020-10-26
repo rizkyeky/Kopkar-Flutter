3cdd4e8f83c9603bbe2822a87a5c30607c2ee06b
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-26 20:46:08 +0700

    update page

--- lib/view/page/page.dart
+++ lib/view/page/page.dart
@@ -3,6 +3,7 @@ library page;
 // import 'package:animations/animations.dart';
 // import 'package:fl_chart/fl_chart.dart';
 import 'package:animations/animations.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:kopkar/theme.dart';
 
