877fa37eca8c3e420cec12eafeaf4149437fb15c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-27 08:32:58 +0700

    update page

--- lib/view/page/page.dart
+++ lib/view/page/page.dart
@@ -5,6 +5,7 @@ library page;
 import 'package:animations/animations.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../bloc/bloc.dart';
 import '../../locator.dart';
