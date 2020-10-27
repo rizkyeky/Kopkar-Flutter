ddbce7c7490deeeecd4fe15706bfd943f18a59ba
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-27 12:28:12 +0700

    create tab and card

--- lib/view/page/page_pinjaman.dart
+++ lib/view/page/page_pinjaman.dart
@@ -10,8 +10,65 @@ class PinjamanPage extends Page<PinjamanBloc> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('KOPKAR'),
+          centerTitle: true,
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          bottom: const TabBar(
+            // isScrollable: true,
+            tabs: [
+              Tab(text: 'BERJALAN',),
+              Tab(text: 'AJUAN',),
+            ]
+          ),
+        ),
+        body: TabBarView(
+          children: List.generate(2, (index) => 
+            ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) => _MyCard()
+            )
+          )
+        )
+      ),
     );
   }
+}
+
+class _MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black.withOpacity(0.12)),
+        borderRadius: BorderRadius.circular(24)
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('Sisa Pinjaman', ),
+                  Text('Rp2.000.000', style: TextStyle(fontWeight: FontWeight.bold),),
+                  SizedBox(height: 24,),
+                  Text('Jumlah Pinjaman', ),
+                  Text('Rp2.000.000', style: TextStyle(fontWeight: FontWeight.bold),)
+                ],
+              )
+            ],
+          ),
+          const Divider(),
+          FlatButton(onPressed: () {}, child: Text('LIHAT DETAIL'))
+        ],
+      ),
+    );
+  }
+
 }
