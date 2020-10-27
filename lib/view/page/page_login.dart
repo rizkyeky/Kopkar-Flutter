dad2cf318ed0cab7a2598e1ce5eb720ae1d9dd5c
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-27 12:27:52 +0700

    delete painting class

--- lib/view/page/page_login.dart
+++ lib/view/page/page_login.dart
@@ -25,7 +25,7 @@ class LoginPage extends Page<LoginBloc> {
               ),
               const SizedBox(height: 24,),
               Padding(
-                padding: const EdgeInsets.all(24),
+                padding: const EdgeInsets.fromLTRB(24, 24, 24, 0),
                 child: XTextField(
                   controller: _emailText,
                   text: 'Email',
@@ -43,7 +43,8 @@ class LoginPage extends Page<LoginBloc> {
               FloatingActionButton.extended(
                 elevation: 0,
                 highlightElevation: 0,
-                onPressed: () {}, 
+                onPressed: () => Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => MainPage())), 
                 label: Row(
                   children: const [
                     Text('Login', style: TextStyle(fontSize: 24)),
@@ -58,48 +59,4 @@ class LoginPage extends Page<LoginBloc> {
       ),
     );
   }
-}
-
-class LoginBackgroundPainter extends CustomPainter {
-  
-  const LoginBackgroundPainter();
-  
-  @override
-  void paint(Canvas canvas, Size size) {
-
-    final path = Path();
-    var paint = Paint();
-
-    path.moveTo(0, 0);
-    path.moveTo(size.width, 0);
-    path.moveTo(size.width, size.height);
-    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
-        size.width * 0.5, size.height * 0.9167);
-    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
-        size.width * 1.0, size.height * 0.9167);
-    // path.lineTo(0, size.height);
-    path.lineTo(0, size.height);
-
-    final colors = [Colors.blue, Colors.white];
-    final stops = [0 / 2, 1.8 / 2];
-
-    final Rect rectShape = Rect.fromLTWH(0, 0, size.width, size.height);
-    final Gradient gradient = LinearGradient(
-      colors: colors, 
-      stops: stops,
-      begin: Alignment.topCenter,
-      end: Alignment.bottomCenter
-    );
-
-    paint = Paint()..shader = gradient.createShader(rectShape);
-
-    canvas.drawPath(path, paint);
-  }
-
-  
-    @override
-    bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
-  }
-
 }
