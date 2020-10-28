01e97c8d5d57fc0e66e298661db89c9024c4b9fd
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-10-28 07:08:07 +0700

    add widgets

--- lib/view/page/page_history.dart
+++ lib/view/page/page_history.dart
@@ -8,10 +8,39 @@ class HistoryPage extends Page<HistoryBloc> {
   @override
   void init() {}
 
+  final List<String> _options = [
+    'Riwayat Pembelian Toko',
+    'Riwayat PPBO',
+    'Riwayat Pinjaman',
+    'Riwayat Potongan Bulanan',
+    'Riwayat SHU',
+    'Riwayat Simpan Wajib'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('KOPKAR'),
+      ),
+      body: ListView.builder(
+        itemCount: _options.length,
+        itemBuilder: (context, index) => Material(
+          child: InkWell(
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.fromLTRB(24, 24, 24, 0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                border: Border.all(color: borderColor)
+              ),
+              child: Text(_options[index]),
+            ),
+          ),
+        ),
+      )
     );
   }
 }
