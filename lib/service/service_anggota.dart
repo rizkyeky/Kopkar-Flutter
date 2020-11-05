f76d03161b2f1ceb88d3335e702ecc38fedde642
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-05 15:30:18 +0700

    fix and update anggota service

--- lib/service/service_anggota.dart
+++ lib/service/service_anggota.dart
@@ -7,7 +7,7 @@ class UserService {
   
   final http.Client _client = http.Client();
   
-  Future<void> getUser() async {
+  Future<void> getAnggota() async {
     
     final Uri _uri = Uri.http(_homeBase, _subBase);
     
@@ -18,21 +18,9 @@ class UserService {
     final bool isLogin = data['message'] == 'Login Berhasil';
 
     if (response.statusCode != 200 || !isLogin) {
-      // Respone not success
       return Anggota.initial();
     } else {
-      return Anggota(
-        data['id_card'] as String,
-        data['nama'] as String,
-        email: data['email'] as String,
-        alamat: data['alamat'] as String,
-        noTelp: data['no_telp'] as String,
-        statusAnggota: data['status_anggota'] == 'AKTIVE',
-        simPokok: (data['sim_pokok'] as num).toInt(),
-        simWajib: (data['sim_wajib'] as num).toInt(),
-        gaji: (data['gaji'] as num).toInt(),
-        batasPinjaman: (data['batas_pinjaman'] as num).toInt()
-      );
+      return Anggota.fromJSON(data as Map<String, dynamic>);
     }
   }
 }
