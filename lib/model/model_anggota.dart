06d0a2ff0ad33847f7e7792cb13760c1302bce27
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-05 15:30:05 +0700

    add fromJSON method

--- lib/model/model_anggota.dart
+++ lib/model/model_anggota.dart
@@ -38,6 +38,20 @@ class Anggota {
     batasPinjaman: 0,
   );
 
+  factory Anggota.fromJSON(Map<String, dynamic> data) => Anggota(
+    data['id_card'] as String,
+    data['nama'] as String,
+    email: data['email'] as String,
+    jenisKel: data['jenis_kel'] as String,
+    alamat: data['alamat'] as String,
+    noTelp: data['no_telp'] as String,
+    statusAnggota: data['status_anggota'] == 'AKTIVE',
+    simPokok: (data['sim_pokok'] as num).toInt(),
+    simWajib: (data['sim_wajib'] as num).toInt(),
+    gaji: (data['gaji'] as num).toInt(),
+    batasPinjaman: (data['batas_pinjaman'] as num).toInt()
+  );
+
   void duplicate(Anggota anggota) {
     idCar = anggota.idCar;
     nama = anggota.nama;
@@ -50,5 +64,31 @@ class Anggota {
     simPokok = anggota.simPokok;
     gaji = anggota.gaji;
     batasPinjaman = anggota.batasPinjaman;
+  }
+
+  void copyWith({
+    String idCar,
+    String nama,
+    String email,
+    String jenisKel,
+    String alamat,
+    String noTelp,
+    bool statusAnggota,
+    int simWajib,
+    int simPokok,
+    int gaji,
+    int batasPinjaman,
+  }) {
+    this.idCar = idCar ?? this.idCar;
+    this.nama = nama ?? this.nama;
+    this.email = email ?? this.email;
+    this.jenisKel = jenisKel ?? this.jenisKel;
+    this.alamat = alamat ?? this.alamat;
+    this.noTelp = noTelp ?? this.noTelp;
+    this.statusAnggota = statusAnggota ?? this.statusAnggota;
+    this.simWajib = simWajib ?? this.simWajib;
+    this.simPokok = simPokok ?? this.simPokok;
+    this.gaji = gaji ?? this.gaji;
+    this.batasPinjaman = batasPinjaman ?? this.batasPinjaman;
   }
 }
