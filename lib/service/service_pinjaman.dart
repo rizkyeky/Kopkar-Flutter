35034a8951c7234593d12e96d0c4ebbd8c7edc47
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-06 20:53:22 +0700

    add method pinjaman service

--- lib/service/service_pinjaman.dart
+++ lib/service/service_pinjaman.dart
@@ -28,7 +28,7 @@ class PinjamanService {
     }
   }
 
-  Future<void> getPinjamanAjuan(String nik) async {
+  Future<List<PinjamanAjuan>> getPinjamanAjuan(String nik) async {
     
     const String _homeBase = '185.210.144.158';
     const String _subBase = 'koperasi_api/Api_v1/getListPengajuanPinjaman';
@@ -40,14 +40,20 @@ class PinjamanService {
       .timeout(const Duration(seconds: 10));
 
     final Map data = json.decode(response.body) as Map;
-    final bool isLogin = data['message'] == 'Login Berhasil';
+    final List result = data['result'] as List;
+
+    final bool isLogin = data['status'] == '200';
 
     if (response.statusCode != 200 || !isLogin) {
+      return [];
+
     } else {
+      return List.generate(result.length, (index) => 
+        PinjamanAjuan.fromJSON(result[index] as Map<String, dynamic>));
     }
   }
 
-  Future<void> getDetailPinjamanBerjalan(String docNo) async {
+  Future<List<Map>> getDetailPinjamanBerjalan(String docNo) async {
     const String _homeBase = '185.210.144.158';
     const String _subBase = 'koperasi_api/api_v1/getDetailAngsuranBerjalan';
     final Uri _uri = Uri.http(_homeBase, _subBase);
@@ -58,10 +64,34 @@ class PinjamanService {
       .timeout(const Duration(seconds: 10));
 
     final Map data = json.decode(response.body) as Map;
-    final bool isLogin = data['message'] == 'Login Berhasil';
+    final List<Map> result = (data['result'] as List)
+      .map((e) => Map.from(e as Map)).toList();
 
-    if (response.statusCode != 200 || !isLogin) {
+    if (response.statusCode != 200) {
+      return [];
     } else {
+      return result;
+    }
+  }
+
+  Future<List<Map>> getDetailPinjamanAngsuran(String docNo) async {
+    const String _homeBase = '185.210.144.158';
+    const String _subBase = 'koperasi_api/api_v1/getDetailAngsuranBerjalan';
+    final Uri _uri = Uri.http(_homeBase, _subBase);
+    
+    final http.Response response = await _client.post(_uri, body: {
+      'doc_no': docNo
+    })
+      .timeout(const Duration(seconds: 10));
+
+    final Map data = json.decode(response.body) as Map;
+    final List<Map> result = (data['result'] as List)
+      .map((e) => Map.from(e as Map)).toList();
+
+    if (response.statusCode != 200) {
+      return [];
+    } else {
+      return result;
     }
   }
 }
