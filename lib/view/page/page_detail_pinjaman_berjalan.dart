29a4ed5f0e763f66a05e1c35f3ecb20a204fa920
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-06 20:54:02 +0700

    add table in detail pinjaman

--- lib/view/page/page_detail_pinjaman_berjalan.dart
+++ lib/view/page/page_detail_pinjaman_berjalan.dart
@@ -1,6 +1,10 @@
 part of 'page.dart';
 
-class DetailPinjamanBerjalanPage extends Page<DetailPinjamanBerjalanBloc> {
+class DetailPinjamanPage extends Page<DetailPinjamanBloc> {
+
+  final String docNo;
+
+  DetailPinjamanPage(this.docNo);
 
   @override
   void dispose() {
@@ -14,6 +18,32 @@ class DetailPinjamanBerjalanPage extends Page<DetailPinjamanBerjalanBloc> {
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      appBar: AppBar(),
+      body: FutureBuilder<List<Map>>(
+        future: _bloc.getTable(docNo),
+        builder: (context, snapshot) {
+          final List<Map> data = snapshot.data ?? [];
+          final List column = data[0].keys.toList();
+          return (snapshot.hasData) ? SingleChildScrollView(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                columns: List.generate(column.length, (indexCol) => 
+                  DataColumn(label: Text(column[indexCol] as String))),
+                rows: List.generate(data.length, (indexRow) {
+                  final List item = data[indexRow].values.toList();
+                  return DataRow(
+                    cells: List.generate(column.length, (indexCell) => 
+                      DataCell(Text(item[indexCell] as String))
+                    )
+                  );
+                })
+              ),
+            ),
+          ) : const Center(child: CircularProgressIndicator(),);
+        }
+      ),
+    );
   }
 }
