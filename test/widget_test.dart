84d5fb68dd66d2f7bb97089ae5444ca16ffe9c0b
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-06 14:04:50 +0700

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,11 +8,18 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:kopkar/service/service.dart';
+import 'package:kopkar/model/model.dart';
+import 'package:kopkar/service/service.dart';
 
 Future<void> main() async {
 
-  // final UserService _service = UserService();
-  // await _service.getUser();
+  final PinjamanService _service = PinjamanService();
+  // await _service.getPinjamanBerjalan('07380');
+  List<PinjamanBerjalan> pinjamanBerjalan = await _service.getPinjamanBerjalan('07380');
+  
+  pinjamanBerjalan.forEach((element) {
+    print(element.docNo);
+    print(element.docDate);
+  });
   // await _service.getSome();
 }
