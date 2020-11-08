f27d0959cfd8c850670d4fdd1415bd2f943390cf
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-08 07:08:41 +0700

    update service

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,6 +1,9 @@
 
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:http/http.dart' as http;
 
 import '../model/model.dart';
@@ -8,3 +11,4 @@ import '../model/model.dart';
 
 part 'service_anggota.dart';
 part 'service_pinjaman.dart';
+part 'service_connectivity.dart';
