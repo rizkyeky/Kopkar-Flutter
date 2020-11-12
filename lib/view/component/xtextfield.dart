4b45e1c00169e357b1230babbb585aa74b2899a5
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-12 18:54:37 +0700

    modify xtextfield component

--- lib/view/component/xtextfield.dart
+++ lib/view/component/xtextfield.dart
@@ -3,51 +3,53 @@ part of 'component.dart';
 class XTextField extends StatelessWidget {
 
   final String text;
-  final TextEditingController controller;
   final bool obscureText;
-  final bool readOnly;
   final Widget suffixIcon;
-  final void Function(String) onChanged;
   final void Function(String) onSubmitted;
   final TextInputType keyboardType;
+  final EdgeInsets contentPadding;
+  final String hintText;
 
   const XTextField({ 
   Key key,
   this.text,
-  this.controller,
   this.obscureText,
   this.suffixIcon,
-  this.onChanged,
   this.onSubmitted,
-  this.readOnly,
-  this.keyboardType
+  this.keyboardType,
+  this.contentPadding,
+  this.hintText,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       keyboardType: keyboardType ?? TextInputType.text,
-      onChanged: onChanged ?? (value) {},
       onSubmitted: onSubmitted ?? (value) {},
       obscureText: obscureText ?? false,
-      controller: controller,
-      readOnly: readOnly ?? false,
+      
       decoration: InputDecoration(
+        contentPadding: contentPadding ?? const EdgeInsets.symmetric(
+          horizontal: 12
+        ),
+        hintText: hintText,
+        hintStyle: const TextStyle(
+          color: Colors.grey
+        ),
         labelText: text,
         suffixIcon: suffixIcon,
         floatingLabelBehavior: FloatingLabelBehavior.never,
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6),
+          borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide(
             color: primaryColor,
             width: 3
           )
         ),
         enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(6),
+          borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide(
             color: borderColor,
-            width: 3
           )
         ),
       ),
