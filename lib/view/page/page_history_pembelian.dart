4e7c634cb1237ff818e95212d7661df6943c9729
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-16 23:33:11 +0700

    create card items in pembelian history

--- lib/view/page/page_history_pembelian.dart
+++ lib/view/page/page_history_pembelian.dart
@@ -13,26 +13,37 @@ class HistoryPembelianPage extends Page<HistoryPembelianBloc> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-      itemCount: 3,
-      itemBuilder: (context, index) => XBox(
-        child: SizedBox(
-          height: 150,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: List.generate(1, (index) => Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('', 
-                  style: appTheme.textTheme.subtitle1),
-                Text('', 
-                  style: appTheme.textTheme.subtitle2.copyWith(fontWeight: FontWeight.bold)),
-              ]
-            ))
-          ),
-        ),
-      ),),
+      body: FutureBuilder<List<Map>>(
+        future: _bloc.getItems(),
+        builder: (context, snapshot) => 
+          (snapshot.hasData) ? (snapshot.data.isNotEmpty) ? ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, index) {
+            final Map item = snapshot.data[index];
+            return XBox(
+            child: InkWell(
+              onTap: () {},
+              child: SizedBox(
+                height: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: List.generate(item.length, (indexCol) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(item.keys.toList()[indexCol] as String,
+                        style: appTheme.textTheme.subtitle1),
+                      Text(item.values.toList()[indexCol] as String, 
+                        style: appTheme.textTheme.subtitle2.copyWith(fontWeight: FontWeight.bold)),
+                    ]
+                  ))
+                ),
+              ),
+            ),
+          );
+          },) : const Center(child: Text('Data tidak ditemukan'),) 
+          : const Center(child: CircularProgressIndicator(),)
+      ),
     );
   }
 }
