f620f4ac4d1b49f6fbaa7072c96f058bfbb454e7
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-17 09:36:11 +0700

    fix and update history pembelian page

--- lib/view/page/page_history_pembelian.dart
+++ lib/view/page/page_history_pembelian.dart
@@ -13,34 +13,52 @@ class HistoryPembelianPage extends Page<HistoryPembelianBloc> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Riwayat Pembelian'),
+      ),
       body: FutureBuilder<List<Map>>(
         future: _bloc.getItems(),
         builder: (context, snapshot) => 
-          (snapshot.hasData) ? (snapshot.data.isNotEmpty) ? ListView.builder(
+          (snapshot.hasData) ? (snapshot.data.isNotEmpty) ? ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(height: 12),  
+          padding: const EdgeInsets.all(24),
           itemCount: snapshot.data.length,
           itemBuilder: (context, index) {
             final Map item = snapshot.data[index];
-            return XBox(
-            child: InkWell(
-              onTap: () {},
-              child: SizedBox(
-                height: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: List.generate(item.length, (indexCol) => Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(item.keys.toList()[indexCol] as String,
-                        style: appTheme.textTheme.subtitle1),
-                      Text(item.values.toList()[indexCol] as String, 
-                        style: appTheme.textTheme.subtitle2.copyWith(fontWeight: FontWeight.bold)),
-                    ]
-                  ))
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(width: 2, color: Theme.of(context).dividerColor),
+                borderRadius: BorderRadius.circular(12)
+              ),
+              child: InkWell(
+                onTap: () => Navigator.push(context, 
+                  MaterialPageRoute(builder: (context) => 
+                    HistoryPembelianDetailPage(item['doc_no'] as String))),
+                child: SizedBox(
+                  height: 150,
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: List.generate(item.length, (indexCol) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(item.keys.toList()[indexCol] as String,
+                            style: appTheme.textTheme.subtitle1),
+                          Text(item.values.toList()[indexCol] as String, 
+                            style: appTheme.textTheme.subtitle2
+                            .copyWith(fontWeight: FontWeight.bold)),
+                        ]
+                      ))
+                    ),
+                  ),
                 ),
               ),
-            ),
-          );
+            );
           },) : const Center(child: Text('Data tidak ditemukan'),) 
           : const Center(child: CircularProgressIndicator(),)
       ),
