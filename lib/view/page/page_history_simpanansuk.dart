58d7915b27842baf1a29a3edf3421d1a88d43ebc
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-17 21:15:32 +0700

    create and modify history simpanans page

--- lib/view/page/page_history_simpanansuk.dart
+++ lib/view/page/page_history_simpanansuk.dart
@@ -10,83 +10,150 @@ class HistorySimpananSukPage extends Page<HistorySimpananBloc> {
   void init() {
   }
 
+  final ValueNotifier<bool> cariNotifier = ValueNotifier(null);
+
   @override
   Widget build(BuildContext context) {
+
+    DateTime pickedAwalDate;
+    DateTime pickedAkhirDate;
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riwayat Simpanan Sukarela'),
+        elevation: 0,
+      ),
       body: ListView(
-      children: [
-        XBox(
-          padding: const EdgeInsets.fromLTRB(12, 12, 12, 6),
-          margin: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Tanggal Awal'),
-              const SizedBox(height: 6,),
-              XTextField(
-                onChanged: (val) {},
-                hintText: 'Contoh: 2020-12-31',
-              ),
-              const SizedBox(height: 12,),
-              const Text('Tanggal Akhir'),
-              const SizedBox(height: 6,),
-              XTextField(
-                onChanged: (val) {},
-                hintText: 'Contoh: 2021-12-31',
-              ),
-              const SizedBox(height: 12,),
-              FlatButton(
-                color: primaryColor,
-                onPressed: () {},
-                child: const Text('CARI', style: TextStyle(color: Colors.white),)
-              )
-            ],
-          )
-        ),
-        const Divider(),
-        
-        ValueListenableBuilder<bool>(
-          valueListenable: null,
+        padding: const EdgeInsets.all(16),
+        children: [
+          XBox(
+            margin: const EdgeInsets.all(0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Tanggal Awal'),
+                        const SizedBox(height: 6,),                
+                        StatefulBuilder(
+                          builder: (context, setState) => FlatButton(
+                            color: primaryColor,
+                            onPressed:() async {
+                              pickedAwalDate = await showDatePicker(
+                                context: context, 
+                                initialDate: DateTime.now(), 
+                                firstDate: DateTime(2000), 
+                                lastDate: DateTime(2025),
+                                initialEntryMode: DatePickerEntryMode.input
+                              );
+                              if (pickedAwalDate != null && pickedAwalDate != _bloc.inputAwalDate) {
+                                setState(() => _bloc.inputAwalDate = pickedAwalDate);
+                              }
+                            },
+                            child: Text(pickedAwalDate != null ? 
+                              _bloc.inputAwalDate.toLocal().toString().split(' ')[0] 
+                              : 'Select Date', style: const TextStyle(color: Colors.white)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 12,),                
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Tanggal Akhir'),
+                        const SizedBox(height: 6,),
+                        StatefulBuilder(
+                          builder: (context, setState) => FlatButton(
+                            color: primaryColor,
+                            onPressed:() async {
+                              pickedAkhirDate = await showDatePicker(
+                                context: context, 
+                                initialDate: DateTime.now(), 
+                                firstDate: DateTime(2000), 
+                                lastDate: DateTime(2025),
+                                initialEntryMode: DatePickerEntryMode.input
+                              );
+                              if (pickedAkhirDate != null && pickedAkhirDate != _bloc.inputAkhirDate) {
+                                setState(() => _bloc.inputAkhirDate = pickedAkhirDate);
+                              }
+                            },
+                            child: Text(pickedAkhirDate != null ? 
+                              _bloc.inputAkhirDate.toLocal().toString().split(' ')[0] 
+                              : 'Select Date', style: const TextStyle(color: Colors.white)),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                FlatButton.icon(
+                  color: primaryColor,
+                  icon: const Icon(Icons.search, color: Colors.white),
+                  onPressed: () => cariNotifier.value = cariNotifier.value != null ? !cariNotifier.value : cariNotifier.value != null,
+                  label: const Text('CARI', style: TextStyle(color: Colors.white),)
+                )
+              ],
+            )
+          ),
+          const Divider(),
+          ValueListenableBuilder<bool>(
+          valueListenable: cariNotifier,
           builder: (context, value, child) => (value != null) ? FutureBuilder<List<Map>>(
-            future: null,
-            builder: (context, snapshot) => (snapshot.hasData) ? (snapshot.data.isNotEmpty) ? Column(
+            future: _bloc.getListS(),
+            builder: (context, snapshot) => 
+              snapshot.connectionState == ConnectionState.done 
+              ? snapshot.data.isNotEmpty ? Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: List.generate(snapshot.data.length, (indexContent) {
-                final List<Map> content = snapshot.data;
-                return XBox(
-                child: SizedBox(
-                  height: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: List.generate(content[indexContent].length, (indexItem) {
-                      final Map<String, String> item = Map<String, String>.from(content[indexContent]);
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(item.keys.toList()[indexItem] ?? '', 
-                            style: appTheme.textTheme.subtitle1),
-                          Text(item.values.toList()[indexItem] ?? '', 
-                            style: appTheme.textTheme.subtitle2.copyWith(fontWeight: FontWeight.bold)),
-                        ]
-                      );
-                    })
+                  final Map item = snapshot.data[indexContent];
+                return Card(
+                  margin: const EdgeInsets.only(bottom: 16),
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Theme.of(context).dividerColor),
+                    borderRadius: BorderRadius.circular(12)
                   ),
-                ),
-              );
+                  child: InkWell(
+                    onTap: () {},
+                    child: SizedBox(
+                      height: 300,
+                      child: Padding(
+                        padding: const EdgeInsets.all(24),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: List.generate(item.length, (indexCol) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(item.keys.toList()[indexCol] as String ?? 'null',
+                                style: appTheme.textTheme.subtitle1),
+                              Text(item.values.toList()[indexCol] as String ?? 'null', 
+                                style: appTheme.textTheme.subtitle2
+                                .copyWith(fontWeight: FontWeight.bold)),
+                            ]
+                          ))
+                        ),
+                      ),
+                    ),
+                  ),
+                );
               })
             ) : const SizedBox(
                 height: 300,
-                child: Center(child: Text('Tidak Ada Data'),))
+                child: Center(child: Text('Data tidak ditemukan'),))
               : const SizedBox(
               height: 300,
               child: Center(child: CircularProgressIndicator(),)
             )
           ) : const Center(),
         )
-
-      ],
-    ),
+        ],
+      ),
     );
   }
 }
