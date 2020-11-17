30d9fb31c8924ab7c9187e99fd15c98207602363
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-17 19:34:39 +0700

    test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -17,11 +17,8 @@ Future<void> main() async {
   final HistoryService _service = HistoryService();
   // await _service.getPinjamanBerjalan('07380');
   // List<PinjamanBerjalan> pinjamanBerjalan = await _service.getPinjamanBerjalan('07380');
-  List<Map> data = await _service.getRiwayat(HistoryType.simpananwajib, 
-    nik: '01739',
-    dateAwal : '2020-01-01',
-    dateAkhir : '2020-07-30',
-  );
+  List<Map> data = await _service.getRiwayat(HistoryType.potongan, 
+      nik:'01739', dateAwal: '2020-01-01', dateAkhir: '2020-07-30');
   
   print(data.length);
   data.forEach((element) {
