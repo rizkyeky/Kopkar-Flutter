dbf07d9460e161505fb8f91a7d027036265c15e4
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-18 08:20:59 +0700

    modify and fix service anggota

--- lib/service/service_anggota.dart
+++ lib/service/service_anggota.dart
@@ -3,24 +3,28 @@ part of 'service.dart';
 class UserService {
 
   final String _homeBase = '185.210.144.158';
-  final String _subBase = 'koperasi_api/api_v1/get_login';
+  final String _subBase = 'koperasi_api/api/account/getLogin';
   
   final http.Client _client = http.Client();
   
-  Future<Anggota> getAnggota() async {
+  Future<Anggota> login(String nik, String password) async {
     
     final Uri _uri = Uri.http(_homeBase, _subBase);
     
-    final http.Response response = await _client.get(_uri)
+    final http.Response response = await _client.post(_uri, body: {
+      'nik': nik,
+      'password': password
+    })
       .timeout(const Duration(seconds: 10));
 
     final Map data = json.decode(response.body) as Map;
-    final bool isLogin = data['message'] == 'Login Berhasil';
+    final bool isSuccess = data['status'] == '200';
+    final List<Map> result = List<Map>.from(data['data'] as List);
 
-    if (response.statusCode != 200 || !isLogin) {
+    if (response.statusCode != 200 || !isSuccess) {
       return Anggota.initial();
     } else {
-      return Anggota.fromJSON(data as Map<String, dynamic>);
+      return Anggota.fromJSON(result as Map<String, dynamic>);
     }
   }
 }
