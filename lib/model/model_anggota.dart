c3581ed1b3e1821f2b31fede2d3400ee138641fe
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2020-11-18 08:11:07 +0700

    modify model anggota

--- lib/model/model_anggota.dart
+++ lib/model/model_anggota.dart
@@ -1,24 +1,20 @@
 part of 'model.dart';
 
 class Anggota {
-  String idCar;
-  String nama;
+  String id;
+  String nikKar;
+  String namaKar;
   String email;
-  String jenisKel;
-  String alamat;
   String noTelp;
-  bool statusAnggota;
   int simWajib;
   int simPokok;
   int gaji;
-  int batasPinjaman;
+  bool batasPinjaman;
 
-  Anggota(this.idCar, this.nama, {
+  Anggota(this.id, this.nikKar, {
+    this.namaKar,
     this.email, 
-    this.alamat,
-    this.statusAnggota,
     this.gaji,
-    this.jenisKel,
     this.noTelp,
     this.simPokok,
     this.simWajib,
@@ -27,39 +23,31 @@ class Anggota {
 
   factory Anggota.initial() => Anggota(
     '', '',
+    namaKar: '',
     email: '',
-    alamat: '',
-    statusAnggota: false,
-    jenisKel: '',
     noTelp: '',
     gaji: 0,
     simPokok: 0,
     simWajib: 0,
-    batasPinjaman: 0,
+    batasPinjaman: false,
   );
 
   factory Anggota.fromJSON(Map<String, dynamic> data) => Anggota(
-    data['id_card'] as String,
-    data['nama'] as String,
+    (data['id'] as num).toString(),
+    data['nik_kar'] as String,
+    namaKar: data['nama_kar'] as String,
     email: data['email'] as String,
-    jenisKel: data['jenis_kel'] as String,
-    alamat: data['alamat'] as String,
     noTelp: data['no_telp'] as String,
-    statusAnggota: data['status_anggota'] == 'AKTIVE',
     simPokok: (data['sim_pokok'] as num).toInt(),
     simWajib: (data['sim_wajib'] as num).toInt(),
     gaji: (data['gaji'] as num).toInt(),
-    batasPinjaman: (data['batas_pinjaman'] as num).toInt()
+    batasPinjaman: data['batas_pinjaman'] as bool
   );
 
   void duplicate(Anggota anggota) {
-    idCar = anggota.idCar;
-    nama = anggota.nama;
+    namaKar = anggota.namaKar;
     email = anggota.email;
-    jenisKel = anggota.jenisKel;
-    alamat = anggota.alamat;
     noTelp = anggota.noTelp;
-    statusAnggota = anggota.statusAnggota;
     simWajib = anggota.simWajib;
     simPokok = anggota.simPokok;
     gaji = anggota.gaji;
@@ -67,25 +55,17 @@ class Anggota {
   }
 
   void copyWith({
-    String idCar,
-    String nama,
+    String namaKar,
     String email,
-    String jenisKel,
-    String alamat,
     String noTelp,
-    bool statusAnggota,
     int simWajib,
     int simPokok,
     int gaji,
-    int batasPinjaman,
+    bool batasPinjaman,
   }) {
-    this.idCar = idCar ?? this.idCar;
-    this.nama = nama ?? this.nama;
+    this.namaKar = namaKar ?? this.namaKar;
     this.email = email ?? this.email;
-    this.jenisKel = jenisKel ?? this.jenisKel;
-    this.alamat = alamat ?? this.alamat;
     this.noTelp = noTelp ?? this.noTelp;
-    this.statusAnggota = statusAnggota ?? this.statusAnggota;
     this.simWajib = simWajib ?? this.simWajib;
     this.simPokok = simPokok ?? this.simPokok;
     this.gaji = gaji ?? this.gaji;
